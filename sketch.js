e8b521ab1509b9797c8b904f9f807802a54cdc7c
AnishanUthayakumar <104171338+AnishanUthayakumar@users.noreply.github.com>
2022-06-20 23:04:26 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,4 +1,8 @@
-var city, police, thief, dynamite
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+
+var city, police, thief, dynamite, invisibleGround
 var cityImg, police_runningImg, police_deadImg, thief_runningImg, dynamiteImg, dynamite_explodedImg
 var dynamite
 var score = 0
@@ -6,8 +10,8 @@ var score = 0
 function preload(){
 //loaded images
  cityImg = loadImage ("city.jpg")
- police_runningImg = loadImage ("police_running.png")
- police_deadImg = loadImage ("police_dead.png")
+ police_runningImg = loadAnimation ("police_running.png")
+ police_deadImg = loadAnimation ("police_dead.png")
  thief_runningImg = loadImage ("thief.png")
  dynamiteImg = loadImage ("dynamite.png")
  dynamite_explodedImg = loadImage ("dynamite_exploded.png")
@@ -26,31 +30,57 @@ function setup() {
  city.velocityX = -4;
 
  //creating police and thief
- police=createSprite(100,325);
- police.addImage(police_runningImg);
- police.addImage(police_deadImg)
+ police=createSprite(50,325);
+ police.addAnimation("running", police_runningImg);
+ police.addAnimation("dead", police_deadImg)
  police.scale = 0.9
 
  thief=createSprite(300,325);
  thief.addImage(thief_runningImg);
- thief.scale = 0.5
+ thief.scale = 0.5  
+
+ //invisible ground
+ invisibleGround = createSprite(200,390,400,10);
+ invisibleGround.visible = false;
 }
 
 function draw() {
-
+ background(0)
  //displaying score
  text("Score: "+ score, 500,50);
 
+ //add gravity
+ police.velocityY = police.velocityY + 0.8
+
+ //stop police from falling down
+ police.collide(invisibleGround);
+
+ if(gameState === PLAY){
+
  //code to reset the background
- if(city.x < -1000 ){
-    city.x = 1000;
+ if(city.x < -500 ){
+    city.x = city.width/2;
  }
 
+//jump when the space key is pressed
+ if(keyDown("space")&& police.y >= 100) {
+    police.velocityY = -12;
+  }
+
+//console.log (PLAY)
+//console.log (END)
  createDynamite()
 
  if (dynamiteGroup.isTouching(police)) {
+     gameState = END
+ }
+ } else if (gameState === END){
+     console.log(gameState)
+     dynamite.changeAnimation("explode", dynamite_explodedImg)
      city.velocityX = 0
-     police.changeImage(police_deadImg)
+    dynamiteGroup.setVelocityXEach(0)
+    police.changeAnimation("dead", police_deadImg)
+    police.scale = 0.5
  }
 
  drawSprites()
@@ -59,9 +89,10 @@ function draw() {
 function createDynamite() {
     if (frameCount % 200 === 0) {
         dynamite = createSprite (275, 350, 10, 10);
-        dynamite.addImage(dynamiteImg);
+        dynamite.addAnimation("dynamite", dynamiteImg);
+        dynamite.addAnimation("explode", dynamite_explodedImg);
         dynamite.scale=0.2;
-        dynamite.velocityX = -3;
+        dynamite.velocityX = -5;
         dynamite.lifetime = 150;
         dynamiteGroup.add(dynamite)
       }
